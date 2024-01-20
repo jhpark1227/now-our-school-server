e48e48c063020ea501d46d4846fe445d1beecba4
jiss00 <ksksksk729@gmail.com>
2024-01-20 15:33:47 +0900

    이용한 시설물 목록 조회

--- src/main/java/com/example/school/facility/dto/FacilityResponseDTO.java
+++ src/main/java/com/example/school/facility/dto/FacilityResponseDTO.java
@@ -1,4 +1,33 @@
 package com.example.school.facility.dto;
 
+import com.example.school.reservation.dto.ReservationResponseDTO;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
 public class FacilityResponseDTO {
+    //사용자 이용한 시설물 정보 DTO
+    @Builder
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class DetailDTO{
+        Long id;
+        String name;
+        String imageURL;
+        String time;
+        String location;
+        Float score;
+    }
+    @Builder
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class DetailResultDTO{
+        List<FacilityResponseDTO.DetailDTO> resultList;
+        Integer listSize;
+    }
 }
