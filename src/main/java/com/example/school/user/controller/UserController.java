ca7d8cd0bbc857845318b5764b175b95e81131b2
htw6269 <htw6269@naver.com>
2024-02-01 00:45:01 +0900

    비밀번호 변경 기능 완료, 프로필 이미지 수정 기능 완료

--- src/main/java/com/example/school/user/controller/UserController.java
+++ src/main/java/com/example/school/user/controller/UserController.java
@@ -19,10 +19,12 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.apache.catalina.User;
 import org.springframework.data.domain.Page;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequiredArgsConstructor
@@ -103,16 +105,17 @@ public class UserController {
         return ApiResponse.onSuccess(UserConverter.toCreateInquiryResultDTO(inquiry));
     }
 
-    // 프로필 수정
-    @PutMapping("/profile/update")
-    @Operation(summary = "프로필 수정 API", description = "프로필 정보를 수정하는 API")
+    // 프로필 정보 수정
+    @PutMapping(value = "/update-profile")
     public ApiResponse<UserResponseDTO.UpdateProfileResultDTO> updateProfile(
             @ExistMember @RequestParam(name = "memberId") Long memberId,
-            @RequestBody @Valid UserRequestDTO.UpdateProfileDTO request) {
+            @RequestPart(value = "image", required = false) MultipartFile profileImage,
+            @RequestPart UserRequestDTO.UpdateProfileDTO updateProfileReqDTO) {
 
-        Member updatedMember = userCommandService.updateProfile(memberId, request);
-        return ApiResponse.onSuccess(UserConverter.toUpdateProfileResultDTO(updatedMember));
+            Member updatedMember = userCommandService.updateProfile(memberId,updateProfileReqDTO, profileImage);
+            return ApiResponse.onSuccess(UserConverter.toUpdateProfileResultDTO(updatedMember));
     }
+
 
     @GetMapping("/info")
     public ApiResponse<UserResponseDTO.Info> getInfo(Authentication auth){
