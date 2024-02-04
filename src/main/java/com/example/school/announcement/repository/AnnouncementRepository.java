7a90e781f094db9e164887fde4248bf17e803c4d
junhyeok <junhyeok0246@naver.com>
2024-02-05 00:46:38 +0900

    feat: 공지사항 갱신 기능 구현

--- src/main/java/com/example/school/announcement/repository/AnnouncementRepository.java
+++ src/main/java/com/example/school/announcement/repository/AnnouncementRepository.java
@@ -6,7 +6,10 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface AnnouncementRepository extends JpaRepository<Announcement, Long>, AnnouncementRepositoryCustom{
     List<Announcement> findBySchoolOrderByCreatedAtDesc(School school, PageRequest page);
+
+    Optional<Announcement> findByTitle(String title);
 }
