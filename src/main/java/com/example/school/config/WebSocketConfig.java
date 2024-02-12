10c32f9d75ed9189f4dabf1483b55921becae07e
htw6269 <htw6269@naver.com>
2024-02-12 16:22:15 +0900

    에러수정

--- src/main/java/com/example/school/config/WebSocketConfig.java
+++ src/main/java/com/example/school/config/WebSocketConfig.java
@@ -19,5 +19,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws").setAllowedOrigins("*").withSockJS();
+        registry.addEndpoint("/stomp").setAllowedOrigins("*").withSockJS();
+
     }
 }
