35254448733d35d1daa9f2fddb2f098f671840e3
junhyeok <junhyeok0246@naver.com>
2024-02-14 23:57:53 +0900

    feat: FacilityTag enum 변경

--- src/main/java/com/example/school/domain/enums/FacilityTag.java
+++ src/main/java/com/example/school/domain/enums/FacilityTag.java
@@ -5,7 +5,7 @@ import lombok.Getter;
 
 @Getter @AllArgsConstructor
 public enum FacilityTag {
-    QUIET("#조용한"), MEETING("#회의"), TEAMPLAY("#팀플"), STUDYROOM("#스터디룸"), PRINT("#프린트");
+    QUIET("#조용한"), ACTIVE("#활기찬"), TEAMPLAY("#팀플"), CROWDED("#사람이 많은"), SECLUDED("#사람이 적은");
 
     private final String tag;
 }
